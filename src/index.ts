6798b0fc26327f01e7d165eb13a86125575c5d76
Tom <tom@weprovide.com>
2019-11-18 09:29:45 +0100

    First test

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,22 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('DEV THIS'));
+
+
+export default class Test {
+  /**
+     * Returns the average of two numbers.
+     *
+     * @remarks
+     * This method is part of the {@link core-library#Statistics | Statistics subsystem}.
+     *
+     * @param x - The first input number
+     * @param y - The second input number
+     * @returns The arithmetic mean of `x` and `y`
+     *
+     * @beta
+     */
+  public static getAverage(x: number, y: number): number {
+    return (x + y) / 2.0;
+  }
+}
